2222a4530243aae4c80fdc4a5ef90638184ec57a
Bernardo Pinheiro <berpinheiroo@gmail.com>
2022-11-07 20:58:35 -0300

    Arrumando o primeiro ex

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
-/*Uma empresa comercial quer um programa para controlar o orçamento de seus
+/* Uma empresa comercial quer um programa para controlar o orçamento de seus
 projetos. Crie uma função para criar um projeto, essa função deve solicitar ao
 usuário o nome do projeto e o seu orçamento total. Crie também uma função que
 recebe 3 parâmetros: o nome do projeto, um valor e o tipo de despesa (“R” - receita
 e “D” - despesa), uma receita deve ser somada ao saldo e uma despesa subtraída
 do saldo, seu código deve impedir uma despesa maior que o saldo disponível e no
-final exibir o saldo atual desse projeto.*/
+final exibir o saldo atual desse projeto. */
 
-/*let nome = prompt("Digite o nome do projeto")
-let orcamento = parseInt(prompt("Qual o orcamento deste projeto"))
-let tipoDespesa = parseInt(prompt("Qual o tipo da despesa? 1 para receita ou 2 para despesa"))
-let valor = parseInt(prompt("Insira um valor para a despesa/receita"))
+let orcamentoTotal = []
+let nomeProjeto = []
+let index = 0
 
-function CriarProjeto(nome, orcamento){
-    console.log("O projeto:", nome, "tem um orcamento de", orcamento, "R$")
-    return orcamento
+function CriarProjeto(){
+    nomeProjeto.push(prompt("Insira o nome do projeto"))
+    orcamentoTotal.push(parseIntprompt("Insira o orcamento total do projeto"))
 }
 
-function CriarProjeto2(nome, valor, tipoDespesa){
-    if(tipoDespesa == "1"){
-        orcamento = orcamento + valor
-        console.log("Apos as receitas, o orcamento do", nome, "e de:", orcamento)
-    } else if(tipoDespesa == "2" && valor < orcamento){
-        orcamento = orcamento - valor
-        console.log("Apos as despesas, o orcamento do", nome, "e de:", orcamento)
-    } else if(valor > orcamento){
-        console.log("Nao tem dinheiro suficiente, faltam", (valor - orcamento), "R$")
+let tipoDespesa
+
+function AtualizarProjeto(nome, valor, tipoDespesa){
+   let index = nomeProjeto.indexOf(nome)
+   if(tipoDespesa == "r"){
+    orcamentoTotal[index] = orcamentoTotal[index] + valor
+   } else if(valor <= orcamentoTotal[index]){
+    orcamentoTotal[index] = orcamentoTotal[index] - valor
+   }
+   console.log(orcamentoTotal[index])
+}
+
+let continuar = "s"
+while(continuar == "s"){
+    let opcao = prompt("Deseja criar ou atualizar um projeto? 1 = criar; 2 = atualizar")
+    if(opcao == "1"){
+        CriarProjeto()
+    } else {
+        let nomeProjeto = prompt("Insira o nome do projeto que deseja atualizar ")
+        let valor = parseInt(prompt("Insira o valor que deseja atualizar"))
+        let despesa = prompt("Insira o tipo de despesa r ou d")
+        AtualizarProjeto(nomeProjeto, valor, despesa)
     }
+    continuar = prompt("Deseja continuar sim ou nao? 's' para sim")
 }
-
-CriarProjeto(nome, orcamento, valor, tipoDespesa)
-CriarProjeto2(nome, valor, tipoDespesa)*/
